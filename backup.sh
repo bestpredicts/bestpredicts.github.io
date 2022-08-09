7e18a3b523c956ab1f4af5e84f426273fb295bb3
speech <speech@ke.com>
2022-08-09 16:50:53 +0800

    Initial backup

--- backup.sh
+++ backup.sh
@@ -5,4 +5,4 @@ git remote add origin https://github.com/bestpredicts/bestpredicts.github.io
 git checkout -b hexo_bkp
 git add .
 git commit -m "Initial backup"
-git push origin hexo_bkp:hexo_bkp
+git push -f origin hexo_bkp:hexo_bkp
